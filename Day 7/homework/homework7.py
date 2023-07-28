fac55f4ed4c92618cc8956d3f803dffe9c0fd0c5
davitjanashia2010 <138299236+davitjanashia2010@users.noreply.github.com>
2023-07-28 15:10:06 +0400

    Add files via upload

---
+++ Day 7/homework/homework7.py
@@ -0,0 +1,4 @@
+age=input("what is your age?: ")
+height=input("what is your height?: ")
+age1=int(age)>int(height)
+print(age1)
---
+++ Day 7/homework/homework77.py
@@ -0,0 +1,4 @@
+memory=input("what is your computers meory?: ")
+ram=input("what is your computers ram?: ")
+memory_ram=int(memory)<int(ram)
+print(memory_ram)
